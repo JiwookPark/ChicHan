0f19b10c6bee686907f482012ff7c34a2cf712c1
JiwookPark <ggorro35@gmail.com>
2024-07-02 13:50:43 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // JavaScript 코드가 여기에 작성됩니다.
+});
+
+function navigateTo(page) {
+    document.querySelectorAll('.page').forEach(section => {
+        section.classList.add('hidden');
+    });
+    document.getElementById(page).classList.remove('hidden');
+}
+
+function showRegionDetails(region) {
+    alert(region + " 지역의 상세 정보를 여기에 표시합니다.");
+}
